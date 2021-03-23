c37b51c383e93b3ddcdae131967fbc7a0fb418ce
Berkay Emre Turhan <berkaytrhn@gmail.com>
2021-03-23 13:40:33 +0300

    initial push, will be completed later

---
+++ question_1.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd 
+
+
+
+def fillEmptyColumns(null_df,df):
+    minValuesGrouped = df.groupby(["country"])["daily_vaccinations"].min().reset_index()
+    print(minValuesGrouped)
+    for country in null_df["country"]:
+        relatedValue = minValuesGrouped.loc[minValuesGrouped["country"] == country]["daily_vaccinations"]
+        print(relatedValue.values[0], "\n********")
+        #minimum değerleri elde ettik, asıl data framede yerine koymak kaldı
+       
+        
+
+
+
+file = pd.read_csv("./country_vaccination_stats.csv",sep=",")
+dataFrame = pd.DataFrame(file)
+
+
+
+
+nullVaccineCountries = dataFrame[dataFrame[dataFrame.columns[2]].isnull()].reset_index()
+print(nullVaccineCountries)
+
+fillEmptyColumns(nullVaccineCountries,dataFrame)
